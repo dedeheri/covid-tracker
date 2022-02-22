d416f3634b174c2d1f4a23bbb95c897dbc7f9013
dedeheri <dedeherii60@gmail.com>
2022-02-22 11:03:35 +0700

    fixed bug map

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -1,56 +1,73 @@
 import React, { useState } from "react";
 
-import ReactMapboxGl, { Marker, Popup } from "react-mapbox-gl";
+// import ReactMapboxGl, { Feature, Layer, Marker, Popup } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxWorker from "mapbox-gl/dist/mapbox-gl-csp-worker";
 
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from "react-map-gl";
+
 import { XIcon } from "@heroicons/react/solid";
-ReactMapboxGl.workerClass = MapboxWorker;
+// ReactMapboxGl.workerClass = MapboxWorker;
 
 const Maps = ({ coordinates }) => {
-  const [popup, setPopup] = useState(null);
+  const [popup, setPopup] = useState(false);
+  const [popups, setPopups] = useState({});
 
-  const Map = ReactMapboxGl({
-    accessToken: "process.env.REACT_APP_MAPBOX_ACCESS_TOKEN",
-  });
+  console.log(popup);
+
+  const mapConfig = {
+    longitude: 106.526,
+    latitude: 0.026,
+    zoom: 2,
+
+    style: {
+      width: "100%",
+      height: "70vh",
+    },
+    key: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+  };
+
   return (
     <Map
-      center={
-        !popup
-          ? [106.526, 0.026]
-          : [popup.countrieInfo.long, popup.countrieInfo.lat]
-      }
-      zoom={[1]}
-      style="mapbox://styles/munnirr/ckzra7gfo000e14nskual5f1w"
-      containerStyle={{
-        height: "50vh",
-        width: "100%",
-      }}
+      initialViewState={{ ...mapConfig }}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      style={mapConfig.style}
+      mapboxAccessToken={mapConfig.key}
     >
       {Object.values(coordinates)?.map((c) => (
         <Marker
           key={c.cases}
-          coordinates={[c.countrieInfo.long, c.countrieInfo.lat]}
+          longitude={c.countrieInfo.long}
+          latitude={c.countrieInfo.lat}
         >
           <button
-            className="bg-red-300  h-5 w-5 rounded-full brightness-50 opacity-50"
-            onClick={(e) => {
-              e.preventDefault();
-              setPopup(c);
-            }}
-          ></button>
+            className="bg-blue-400  h-5 w-5 rounded-full brightness-50 opacity-50"
+            onClick={() => setPopup(c)}
+          />
         </Marker>
       ))}
 
       {popup ? (
-        <Popup coordinates={[popup.countrieInfo.long, popup.countrieInfo.lat]}>
+        <Popup
+          longitude={popup.countrieInfo.long}
+          latitude={popup.countrieInfo.lat}
+          closeButton={false}
+          closeOnClick={false}
+        >
           <div>
             <div className="flex justify-between">
               <h1 className="font-bold text-md">{popup.countrieName}</h1>
 
               <button
                 onClick={() => {
-                  setPopup(null);
+                  setPopup(false);
                 }}
               >
                 <XIcon className="w-4" />
@@ -74,7 +91,9 @@ const Maps = ({ coordinates }) => {
             </div>
           </div>
         </Popup>
-      ) : null}
+      ) : (
+        false
+      )}
     </Map>
   );
 };
